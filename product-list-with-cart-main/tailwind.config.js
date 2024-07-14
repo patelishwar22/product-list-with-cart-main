e32d140d0d1fd8ff441af44d2d7f0874ac75ec0d
ishupatel07 <ishwarrpatel22@gmail.com>
2024-07-14 22:06:05 +0530

    first commit

---
+++ product-list-with-cart-main/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily :{
+        redHatText: ['RedHatText', 'sans-serif'],
+      },
+      colors: {
+        red: 'hsl(14, 86%, 42%)',
+        green: 'hsl(159, 69%, 38%)',
+        rose: {
+          50: 'hsl(20, 50%, 98%)',
+          100: 'hsl(13, 31%, 94%)',
+          300: 'hsl(14, 25%, 72%)',
+          400: 'hsl(7, 20%, 60%)',
+          500: 'hsl(12, 20%, 44%)',
+          900: 'hsl(14, 65%, 9%)',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
